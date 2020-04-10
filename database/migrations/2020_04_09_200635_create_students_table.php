9ed0d0c2d577bede3029f8aeaf151fba2c1b3b93
111865 <39704946+111865@users.noreply.github.com>
2020-04-10 23:03:23 +0300

    Add files via upload

---
+++ database/migrations/2020_04_09_200635_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('student_number')->unique();
+            $table->string('full_name');
+            $table->date('date_of_birth');
+            $table->longText('address');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
